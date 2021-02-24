4bf3116a70e79ceda7c84fb777339dcbcd47dd70
hoang.nhat.phan <hoang.nhat.phan@topicus.nl>
2021-02-24 18:51:54 +0700

    update code

--- JourneyControl/JourneyControl.Infrastructure.Persistence/ServiceRegistration.cs
+++ JourneyControl/JourneyControl.Infrastructure.Persistence/ServiceRegistration.cs
@@ -34,6 +34,18 @@ namespace JourneyControl.Infrastructure.Persistence
                     ServiceLifetime.Scoped  //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
                 );
 
+            services
+                .AddDbContext<WMSDBContext>(options =>
+                    {
+                        options.UseSqlServer(configuration.GetConnectionString("WMSDBConnection"),
+                            sqlServerOptionsAction: sqlOptions =>
+                            {
+                                sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                            });
+                    },
+                    ServiceLifetime.Scoped  //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
+                );
+
         }
     }
 }
